f86981e2e868dafbb324620c7523521226496ed9
CHENBIEBIE <921212200@qq.com>
2016-06-13 17:09:29 +0800

    web-04-Log

---
+++ web/src/com/kaishengit/web/test/Log4jTestCase.java
@@ -0,0 +1,27 @@
+package com.kaishengit.web.test;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Administrator on 2016/6/13.
+ */
+public class Log4jTestCase {
+
+    @Test
+    public void testLog(){
+
+        Logger logger = LoggerFactory.getLogger(Log4jTestCase.class);//Logger.getLogger(Log4jTestCase.class);
+        String name = "Alex";
+        String bookname="《重构》";
+
+        logger.trace("trace message");
+        logger.debug("debug message");
+        logger.info("{}借阅了{}",name,bookname);
+        logger.warn("warn message");
+        logger.error("error message");
+        //logger.fatal("fatal message");
+    }
+}
