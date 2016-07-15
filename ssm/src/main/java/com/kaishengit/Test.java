b1ec4d8d5a8d551422cf7570981e4fadac309b4f
CHENBIEBIE <921212200@qq.com>
2016-07-15 12:11:55 +0800

    完成我二维码扫描

--- ssm/src/main/java/com/kaishengit/Test.java
+++ ssm/src/main/java/com/kaishengit/Test.java
@@ -1,6 +1,7 @@
 package com.kaishengit;
 
 
+import com.sun.java.util.jar.pack.Histogram;
 import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
@@ -12,13 +13,7 @@ public class Test {
 
     public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
 
-        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
-        format.setCaseType(HanyuPinyinCaseType.LOWERCASE); //让拼音为小写
-        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE); //不要音调
-        format.setVCharType(HanyuPinyinVCharType.WITH_V); //让u使用v来表示
-        String result = PinyinHelper.toHanYuPinyinString("驴",format,"",true);
 
-        System.out.println(result);
 
 
     }
