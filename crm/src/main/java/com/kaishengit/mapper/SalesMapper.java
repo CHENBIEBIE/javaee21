948d246168df54673f047e84ba9910cc525e6998
CHENBIEBIE <921212200@qq.com>
2016-07-19 20:06:28 +0800

    统计

--- crm/src/main/java/com/kaishengit/mapper/SalesMapper.java
+++ crm/src/main/java/com/kaishengit/mapper/SalesMapper.java
@@ -1,6 +1,7 @@
 package com.kaishengit.mapper;
 
 import com.kaishengit.pojo.Sales;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -25,4 +26,12 @@ public interface SalesMapper {
     void del(Integer id);
 
     List<Sales> findByCustId(Integer custId);
+
+    Long findStateCount(@Param("start") String start ,@Param("end") String end ,@Param("state") String state);
+
+    Float findStateMoney(@Param("start") String start ,@Param("end") String end ,@Param("state") String state);
+
+    List<Map<String,Object>> countProgress(@Param("start") String start ,@Param("end") String end);
+
+    List<Map<String,Object>> totalUserMoney(@Param("start") String start ,@Param("end") String end);
 }
